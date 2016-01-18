a93a537acfee2198f7d347250911cc3fc7fdadde
ngrande <devlup@outlook.com>
2016-01-18 22:57:57 +0100

    improved UI

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
+//		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
